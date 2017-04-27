0362bcbe734d50d58b4741f8601b1f798be06480
susu12138 <2465551714@qq.com>
2017-04-27 11:38:17 +0800

    update1.1 deepwalk

--- deepwalk.py
+++ deepwalk.py
@@ -1,7 +1,275 @@
-import numpy
-import deepwalk
+import numpy as np
+import matplotlib.pyplot as plt
+
 import networkx as nx
+class ReadVec:
+    def __init__(self):
+        self.read_v()
+        self.show()
+    def read_v(self):
+        dic = {}
+        with open("data/node2vec.emb","r") as f:
+            i=1
+            for line in f:
+                if i ==1:
+                    i+=1
+                    continue
+                v=line.split()
+                l=len(v)
+                temp_v=np.zeros(l-1)
+                for i in range(1,l):
+                    temp_v[i-1]=v[i]
+                dic[v[0]]=temp_v
+        return dic
+    def show(self):
+        self.new_dic={}
+        dic=self.read_v()
+        for line in dic:
+            self.new_dic[self.changeToStr(int(line)).upper()]=dic[line]
 
-g=nx.read_gexf("data\\timeline_new_g_l.gexf")
+        self.com_cosine_matrix()
+        print(self.mapping)
+        #print(self.cos)
+        print(self.max)
+        self.year_interve=self.max/14
 
-a=deepwalk(g)
+
+    def changeToStr(self, num):
+        strl = ""
+        a = ord('a')
+        num = int(num)
+        i=1
+        while (i<=6):
+            temp = (num % 100)
+            if temp<=27:
+                temp=temp+a
+            else:
+                print("haha"+str(temp))
+            strl += chr(temp)
+            num = (int)(num / 100)
+            i+=1
+        return strl[::-1]
+    def com_cosine_matrix(self):
+        l=len(self.new_dic)
+        self.mapping={}
+        self.cos=np.zeros((l,l))
+        i=0
+        for item in self.new_dic:
+            self.mapping[item]=i
+            i+=1
+
+        max = 0
+        for x in self.new_dic:
+            for y in self.new_dic:
+                i=self.mapping[x]
+                j=self.mapping[y]
+                self.cos[i,j]=np.sqrt(np.sum(np.square(self.new_dic[x]-self.new_dic[y])))
+                if max<self.cos[i,j]:
+                    max=self.cos[i,j]
+        self.max=max
+class Connection:
+    def cal_connection_node(self,a,interval):
+        g=nx.read_gexf("data/timeline_new_g_l.gexf")
+        year=2002
+        for node in g:
+            index=node[:6].upper()
+
+            t=int(node[6:])
+            g.node[node]["array_1"]=np.asscalar(a[index][0])
+            g.node[node]["array_2"]=np.asscalar(a[index][1])
+            g.node[node]["array_3"]=np.asscalar((t-year)*interval)
+        nx.write_gexf(g,"data/timeline_new_g_l_node2vec.gexf")
+def c():
+    b=ReadVec()
+    print(b.new_dic)
+    a=Connection()
+    interval=b.max/25
+    a.cal_connection_node(b.new_dic,interval)
+
+class Conn:
+    def __init__(self,path="data/timeline_new_g_l_node2vec.gexf"):
+        self.g=nx.read_gexf(path)
+        self.path=path
+    def relabel(self,strl):
+        temp=strl[:6].upper()
+        temp+=strl[6:]
+        return temp
+    def relabel_net(self):
+        mapping = {k:self.relabel(k) for k in self.g.nodes()}
+        new_g=nx.relabel_nodes(self.g,mapping)
+        self.g=new_g
+        nx.write_gexf(self.g,self.path)
+
+    def core(self):
+        for node in self.g:
+            self.g.node[node]["is_cal"]=False
+        for node in self.g:
+            if self.g.node[node]["is_cal"]:
+                continue
+            else:
+                year=int(node[6:])
+                self.g.node[node]["is_cal"]=True
+                pre_vec=self.find_forward_node(node)
+                bak_vec=self.find_backward_node(node)
+                pre_node=node[:6]+str(year-1)
+                if self.g.has_node(pre_node):
+                    p_vec=self.find_forward_node(pre_node)
+                    b_vec=self.find_backward_node(pre_node)
+                else:
+                    continue
+                ans=0
+                for i in range(0,3):
+                    if p_vec[i] == pre_node or pre_vec[i]==node:
+                        continue
+                    a = self.g.node[p_vec[i]]["array_1"]-self.g.node[pre_node]["array_1"]
+                    b = self.g.node[p_vec[i]]["array_2"] - self.g.node[pre_node]["array_2"]
+                    c = self.g.node[p_vec[i]]["array_3"] - self.g.node[pre_node]["array_3"]
+                    e = self.g.node[pre_vec[i]]["array_1"] - self.g.node[node]["array_1"]
+                    f = self.g.node[pre_vec[i]]["array_2"] - self.g.node[node]["array_2"]
+                    g = self.g.node[pre_vec[i]]["array_3"] - self.g.node[node]["array_3"]
+                    a=a-e
+                    b=b-f
+                    c=c-g
+                    ans+=(a*a+b*b+c*c)
+                    if pre_node==b_vec[i] or node==bak_vec[i]:
+                        continue
+                    a = self.g.node[pre_node]["array_1"] - self.g.node[b_vec[i]]["array_1"]
+                    b = self.g.node[pre_node]["array_2"] - self.g.node[b_vec[i]]["array_2"]
+                    c = self.g.node[pre_node]["array_3"] - self.g.node[b_vec[i]]["array_3"]
+                    e = self.g.node[node]["array_1"] - self.g.node[bak_vec[i]]["array_1"]
+                    f = self.g.node[node]["array_2"] - self.g.node[bak_vec[i]]["array_2"]
+                    g = self.g.node[node]["array_3"] - self.g.node[bak_vec[i]]["array_3"]
+                    a = a - e
+                    b = b - f
+                    c = c - g
+                    ans += (a * a + b * b + c * c)
+                self.g.node[node]["ans"]=ans
+                print(node,end=" ")
+                print(ans)
+
+        nx.write_gexf(self.g,self.path)
+
+    def display(self,strl):
+        year=2002
+        y=np.zeros(13)
+        for i in range(year,2015):
+            node=strl+str(i)
+            if self.g.has_node(node):
+                try:
+                    y[i-year]=self.g.node[node]["ans"]
+                    print(y[i-year],end=" ")
+                    print(i)
+                except KeyError:
+                    continue
+            else:
+                pass
+        x=np.arange(year,2015,1)
+        return x,y
+
+    def bat_display(self):
+        dic_node={}
+
+        for node in self.g.nodes():
+            item=node[:6]
+            dic_node[item]=1
+        i=1
+        for k in dic_node.keys():
+            if i>9:
+                break
+            x,y=self.display(k)
+            a=plt.subplot(3,3,i)
+            a.set_title(k)
+            a.plot(x,y)
+            i+=1
+
+        plt.show()
+        plt.savefig("data/"+str(i)+"my.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def find_forward_node(self,node):
+        max=[0,-1,-2]
+        max_node=[node,node,node]
+        for i in self.g.predecessors(node):
+            if i==node:
+                continue
+            temp=self.g[i][node]["weight"]
+
+            if temp>max[0]:
+                max[2]=max[1]
+                max[1]=max[0]
+                max[0]=temp
+                max_node[2]=max_node[1]
+                max_node[1]=max_node[0]
+                max_node[0]=i
+            elif temp>max[1]:
+                max[2]=max[1]
+                max[1]=temp
+                max_node[2]=max_node[1]
+                max_node[1]=i
+            elif temp>max[2]:
+                max[2]=temp
+                max_node[2]=i
+        return max_node
+
+
+
+
+    def find_backward_node(self,node):
+        max = [0, -1, -2]
+        max_node = [node, node, node]
+        for i in self.g.successors(node):
+            if i==node:
+                continue
+            temp = self.g[node][i]["weight"]
+
+            if temp > max[0]:
+                max[2] = max[1]
+                max[1] = max[0]
+                max[0] = temp
+                max_node[2] = max_node[1]
+                max_node[1] = max_node[0]
+                max_node[0] = i
+            elif temp > max[1]:
+                max[2] = max[1]
+                max[1] = temp
+                max_node[2] = max_node[1]
+                max_node[1] = i
+            elif temp > max[2]:
+                max[2] = temp
+                max_node[2] = i
+        return max_node
+
+a=Conn()
+a.bat_display()
+
+
+
+
+
+
+
+
+
+
+
+
--- time_r.py
+++ time_r.py
@@ -17,196 +17,302 @@ year={}
 #update
 
 
+class gen_net:
+    def __init(self):
+        pass
 
-
-def scan_csv_get_sub(file_path,g):
-    with open(file_path) as csvfile:
-        file=csv.reader(csvfile)
-        i=1
-        line_pre=""
-        year_pre=0
-        for line in file:
-            i+=1
-            if i<START:
-                continue
-            else:
-                if i%2000==0:
-                    print((i-START)/(total-START))
-                if i%50000==0:
-                    gc.collect()
-
-                try:
-                    data1=get_file_json(line[0])
-                    data2=get_file_json(line[1])
-                    #uniform_sub(data1,data2)
-                    data1["tocSection"]["label"]+=data1["date"][:4]
-                    data2["tocSection"]["label"]+=data2["date"][:4]
-
-
-                except KeyError as err:
-                    #print("scan_file filename key error,pass")
+    def scan_csv_get_sub(file_path,g):
+        with open(file_path) as csvfile:
+            file=csv.reader(csvfile)
+            i=1
+            line_pre=""
+            year_pre=0
+            for line in file:
+                i+=1
+                if i<START:
                     continue
-                except FileNotFoundError as err:
-                    #print("scan file file not find haha")
-                    continue
-
-                #print(data1["date"]+"  "+data2["date"])
-                insert_network(g,data1,data2)
-                if line_pre==line[1]:
-                    continue
-                elif "tocSection" in data2 and g.has_node(data2["tocSection"]["label"]):
-                    g.node[data2["tocSection"]["label"]]["num"]+=1
-                if int(data2["date"][:4]) in year:
-                    year[int(data2["date"][:4])] += 1
                 else:
-                    year[int(data2["date"][:4])] = 1
-                line_pre=line[1]
-        print("-------------------")
-#gennerate real timeline based network
-def gen_timeline_net():
-    s=nx.DiGraph()
-    scan_csv_get_sub(dir_file,s)
-    scan_avgau_and_in_out(s)
-    nx.write_gexf(s,"data\\timeline_test_l.gexf")
-    for item in year:
-        print(str(item)+" "+str(year[item]))
+                    if i%2000==0:
+                        print((i-START)/(total-START))
+                    if i%50000==0:
+                        gc.collect()
+
+                    try:
+                        data1=get_file_json(line[0])
+                        data2=get_file_json(line[1])
+                        #uniform_sub(data1,data2)
+                        data1["tocSection"]["label"]+=data1["date"][:4]
+                        data2["tocSection"]["label"]+=data2["date"][:4]
 
 
-def merge_node(new_g,g,node1):
-    node_name=node1[:6]+node1[-4:]
-    if node_name not in new_g:
-        new_g.add_node(node_name,num=g.node[node1]["num"],sum_author=g.node[node1]["sum_author"],journal=g.node[node1]["journal"])
-        for node in g.predecessors(node1):
-            if g.node[node]["isMerged"]==True:
-                continue
-            node_name2=node[:6]+node[-4:]
-            if new_g.has_edge(node_name2, node_name):
-                new_g[node_name2][node_name]["weight"] += g[node][node1]["weight"]
-            else:
-                new_g.add_edge(node_name2, node_name, weight=g[node][node1]["weight"])
+                    except KeyError as err:
+                        #print("scan_file filename key error,pass")
+                        continue
+                    except FileNotFoundError as err:
+                        #print("scan file file not find haha")
+                        continue
 
-        for node in g.successors(node1):
-            if g.node[node]["isMerged"]==True:
-                continue
-            node_name2=node[:6]+node[-4:]
-            if new_g.has_edge(node_name, node_name2):
-                new_g[node_name][node_name2]["weight"] += g[node1][node]["weight"]
-            else:
-                new_g.add_edge(node_name, node_name2, weight=g[node1][node]["weight"])
-    else:
-        if "num" not in new_g.node[node_name]:
-            print(new_g.node[node_name])
-            new_g.node[node_name]["num"]=g.node[node1]["num"]
-            new_g.node[node_name]["sum_author"]=g.node[node1]["sum_author"]
-            new_g.node[node_name]["journal"] = g.node[node1]["journal"]
+                    #print(data1["date"]+"  "+data2["date"])
+                    insert_network(g,data1,data2)
+                    if line_pre==line[1]:
+                        continue
+                    elif "tocSection" in data2 and g.has_node(data2["tocSection"]["label"]):
+                        g.node[data2["tocSection"]["label"]]["num"]+=1
+                    if int(data2["date"][:4]) in year:
+                        year[int(data2["date"][:4])] += 1
+                    else:
+                        year[int(data2["date"][:4])] = 1
+                    line_pre=line[1]
+            print("-------------------")
+    #gennerate real timeline based network
+    def gen_timeline_net(self):
+        s=nx.DiGraph()
+        scan_csv_get_sub(dir_file,s)
+        scan_avgau_and_in_out(s)
+        nx.write_gexf(s,"data\\timeline_test_l.gexf")
+        for item in year:
+            print(str(item)+" "+str(year[item]))
+
+
+    def merge_node(new_g,g,node1):
+        node_name=node1[:6]+node1[-4:]
+        if node_name not in new_g:
+            new_g.add_node(node_name,num=g.node[node1]["num"],sum_author=g.node[node1]["sum_author"],journal=g.node[node1]["journal"])
+            for node in g.predecessors(node1):
+                if g.node[node]["isMerged"]==True:
+                    continue
+                node_name2=node[:6]+node[-4:]
+                if new_g.has_edge(node_name2, node_name):
+                    new_g[node_name2][node_name]["weight"] += g[node][node1]["weight"]
+                else:
+                    new_g.add_edge(node_name2, node_name, weight=g[node][node1]["weight"])
+
+            for node in g.successors(node1):
+                if g.node[node]["isMerged"]==True:
+                    continue
+                node_name2=node[:6]+node[-4:]
+                if new_g.has_edge(node_name, node_name2):
+                    new_g[node_name][node_name2]["weight"] += g[node1][node]["weight"]
+                else:
+                    new_g.add_edge(node_name, node_name2, weight=g[node1][node]["weight"])
         else:
-            new_g.node[node_name]["num"] += g.node[node1]["num"]
-            new_g.node[node_name]["sum_author"] += g.node[node1]["sum_author"]
-        for node in g.predecessors(node1):
-            if g.node[node]["isMerged"]==True:
-                continue
-            node_name2=node[:6]+node[-4:]
-            if new_g.has_edge(node_name2, node_name):
-                new_g[node_name2][node_name]["weight"] += g[node][node1]["weight"]
+            if "num" not in new_g.node[node_name]:
+                print(new_g.node[node_name])
+                new_g.node[node_name]["num"]=g.node[node1]["num"]
+                new_g.node[node_name]["sum_author"]=g.node[node1]["sum_author"]
+                new_g.node[node_name]["journal"] = g.node[node1]["journal"]
             else:
-                new_g.add_edge(node_name2, node_name, weight=g[node][node1]["weight"])
+                new_g.node[node_name]["num"] += g.node[node1]["num"]
+                new_g.node[node_name]["sum_author"] += g.node[node1]["sum_author"]
+            for node in g.predecessors(node1):
+                if g.node[node]["isMerged"]==True:
+                    continue
+                node_name2=node[:6]+node[-4:]
+                if new_g.has_edge(node_name2, node_name):
+                    new_g[node_name2][node_name]["weight"] += g[node][node1]["weight"]
+                else:
+                    new_g.add_edge(node_name2, node_name, weight=g[node][node1]["weight"])
 
-        for node in g.successors(node1):
-            if g.node[node]["isMerged"]==True:
+            for node in g.successors(node1):
+                if g.node[node]["isMerged"]==True:
+                    continue
+                node_name2=node[:6]+node[-4:]
+                if new_g.has_edge(node_name, node_name2):
+                    new_g[node_name][node_name2]["weight"] += g[node1][node]["weight"]
+                else:
+                    new_g.add_edge(node_name, node_name2, weight=g[node1][node]["weight"])
+        print("delete"+node1)
+
+
+
+
+    #call merge_node function
+    def merge_multiName_net(self):
+
+        g = nx.read_gexf("data\\timeline_test.gexf")
+
+        new_g=nx.DiGraph()
+        for node in g.nodes():
+            g.node[node]["isMerged"]=False
+        for node in g.nodes():
+            if g.node[node]["isMerged"] == True:
                 continue
-            node_name2=node[:6]+node[-4:]
-            if new_g.has_edge(node_name, node_name2):
-                new_g[node_name][node_name2]["weight"] += g[node1][node]["weight"]
+            self.merge_node(new_g,g,node)
+            g.node[node]["isMerged"] = True
+
+        nx.write_gexf(new_g,"data\\timeline_new_g.gexf")
+    def equal_zero(self,a,e=0.000001):
+        if a<e and a>-e:
+            return True
+        else:
+            return False
+
+    #generate one node attribute
+    def gen_avg_cited_year(g,node):
+        weighted_sum=0.0
+        sum=0.0
+        year=int(node[-4:])
+        print(year)
+        for i in g.successors(node):
+            year1=int(i[-4:])
+            sum+=g[node][i]["weight"]
+            weighted_sum+=g[node][i]["weight"]*(year-year1)
+
+        g.node[node]["out"]=sum
+        if self.equal_zero(sum):
+            g.node[node]["avg_year_out"] = 0
+        else:
+            g.node[node]["avg_year_out"] = weighted_sum / sum
+
+
+
+        weighted_sum = 0.0
+        sum = 0.0
+        for i in g.predecessors(node):
+            year1=int(i[-4:])
+            sum+=g[i][node]["weight"]
+            weighted_sum+=g[i][node]["weight"]*(year1-year)
+        g.node[node]["in"]=sum
+        if self.equal_zero(sum):
+            g.node[node]["avg_year_in"] = 0
+        else:
+            g.node[node]["avg_year_in"] = weighted_sum / sum
+
+    def merge_node_without_year(self,new_g, g, node1):
+        node_name = node1[:6]
+        if node_name not in new_g:
+            new_g.add_node(node_name, num=g.node[node1]["num"], sum_author=g.node[node1]["sum_author"],
+                           journal=g.node[node1]["journal"])
+            for node in g.predecessors(node1):
+                if g.node[node]["isMerged"] == True:
+                    continue
+                node_name2 = node[:6]
+                if new_g.has_edge(node_name2, node_name):
+                    new_g[node_name2][node_name]["weight"] += g[node][node1]["weight"]
+                else:
+                    new_g.add_edge(node_name2, node_name, weight=g[node][node1]["weight"])
+
+            for node in g.successors(node1):
+                if g.node[node]["isMerged"] == True:
+                    continue
+                node_name2 = node[:6]
+                if new_g.has_edge(node_name, node_name2):
+                    new_g[node_name][node_name2]["weight"] += g[node1][node]["weight"]
+                else:
+                    new_g.add_edge(node_name, node_name2, weight=g[node1][node]["weight"])
+        else:
+            if "num" not in new_g.node[node_name]:
+                print(new_g.node[node_name])
+                new_g.node[node_name]["num"] = g.node[node1]["num"]
+                new_g.node[node_name]["sum_author"] = g.node[node1]["sum_author"]
+                new_g.node[node_name]["journal"] = g.node[node1]["journal"]
             else:
-                new_g.add_edge(node_name, node_name2, weight=g[node1][node]["weight"])
-    print("delete"+node1)
+                new_g.node[node_name]["num"] += g.node[node1]["num"]
+                new_g.node[node_name]["sum_author"] += g.node[node1]["sum_author"]
+            for node in g.predecessors(node1):
+                if g.node[node]["isMerged"] == True:
+                    continue
+                node_name2 = node[:6]
+                if new_g.has_edge(node_name2, node_name):
+                    new_g[node_name2][node_name]["weight"] += g[node][node1]["weight"]
+                else:
+                    new_g.add_edge(node_name2, node_name, weight=g[node][node1]["weight"])
+
+            for node in g.successors(node1):
+                if g.node[node]["isMerged"] == True:
+                    continue
+                node_name2 = node[:6]
+                if new_g.has_edge(node_name, node_name2):
+                    new_g[node_name][node_name2]["weight"] += g[node1][node]["weight"]
+                else:
+                    new_g.add_edge(node_name, node_name2, weight=g[node1][node]["weight"])
+        print("delete" + node1)
+    def merge_multiName_net_without_year(self,g):
+        new_g=nx.DiGraph()
+        for node in g.nodes():
+            g.node[node]["isMerged"]=False
+        for node in g.nodes():
+            if g.node[node]["isMerged"] == True:
+                continue
+            self.merge_node_without_year(new_g,g,node)
+            g.node[node]["isMerged"] = True
+        return new_g
+
+    #add a new attribute to sub_net_year
+    def gen_avg_cited_net(self):
+        g=nx.read_gexf("data\\timeline_new_g.gexf")
+        for node in g:
+            self.gen_avg_cited_year(g,node)
+
+        nx.write_gexf(g,"data\\timeline_new_g.gexf")
+
+    def gen_group_net(g,new_g,start_g):
+        for node in g.nodes(data=True):
+            if new_g.has_node(node[1]["group"]):
+                new_g.node[node[1]["group"]]["in"]+=node[1]["in"]
+                new_g.node[node[1]["group"]]["out"] += node[1]["out"]
+                new_g.node[node[1]["group"]]["num"] += node[1]["num"]
+                new_g.node[node[1]["group"]]["sum_author"] += node[1]["sum_author"]
+
+            else:
+                new_g.add_node(node[1]["group"], in_=node[1]["in"],out=node[1]["out"],num=node[1]["num"],
+                               weight=node[1]["weight"],label=node[1]["group"])
+        for edge in start_g.edges(data=True):
+           # print(g.node[start_g.node[edge[1]]["label"]])
+            node1=g.node[start_g.node[edge[0]]["label"]]["group"]
+            node2=g.node[start_g.node[edge[1]]["label"]]["group"]
+            if new_g.has_edge(node1,node2):
+                new_g[node1][node2]["weight"]+=edge[2]["weight"]
+            else:
+                new_g.add_edge(node1, node2, weight=edge[2]["weight"])
+
+    def gen_new_net(self):
+        self.merge_multiName_net()
+        self.gen_avg_cited_net()
+    def changeToNum(self,strl):
+        sum=0
+        a = ord('a')
+        z=ord('z')
+        A=ord('A')
+        Z=ord('Z')
+        for i in strl:
+            temp=ord(i)
+            if temp>=a and temp<=z:
+                temp=temp-ord('a')
+            elif temp>=A and temp<=Z:
+                temp=temp-ord('A')
+            sum=sum*100+temp
+        return sum
+    def changeToStr(self,num):
+        strl=""
+        a = ord('a')
+        num=int(num)
+        while(num):
+            temp=(num % 100)+a
+            strl+=chr(temp)
+            num=(int)(num/100)
+        return strl[::-1]
+    def test_hash(self):
+        str="hello"
+        temp=self.changeToNum(str)
+        print(temp)
+        print(self.changeToStr(temp))
+
+    def relabel_net(self,g):
+        mapping={k:self.changeToNum(k) for k in g.nodes()}
+        new_g=nx.relabel_nodes(g,mapping)
+        return new_g
+    def relabel_net_re(self,g):
+        mapping={k:self.changeToStr(k) for k in g.nodes()}
+        new_g=nx.relabel_nodes(g,mapping)
+        return new_g
+    def gen_net_without_year(self):
+        g = nx.read_gexf("data\\timeline_new_g_l.gexf")
+        new_g=self.merge_multiName_net_without_year(g)
+        g=self.relabel_net(new_g)
+        nx.write_gexf(g,"data\\timeline_new_g_l_relabel.gexf")
 
 
-
-
-#call merge_node function
-def merge_multiName_net():
-
-    g = nx.read_gexf("data\\timeline_test.gexf")
-
-    new_g=nx.DiGraph()
-    for node in g.nodes():
-        g.node[node]["isMerged"]=False
-    for node in g.nodes():
-        if g.node[node]["isMerged"] == True:
-            continue
-        merge_node(new_g,g,node)
-        g.node[node]["isMerged"] = True
-
-    nx.write_gexf(new_g,"data\\timeline_new_g.gexf")
-def equal_zero(a,e=0.000001):
-    if a<e and a>-e:
-        return True
-    else:
-        return False
-
-#generate one node attribute
-def gen_avg_cited_year(g,node):
-    weighted_sum=0.0
-    sum=0.0
-    year=int(node[-4:])
-    print(year)
-    for i in g.successors(node):
-        year1=int(i[-4:])
-        sum+=g[node][i]["weight"]
-        weighted_sum+=g[node][i]["weight"]*(year-year1)
-
-    g.node[node]["out"]=sum
-    if equal_zero(sum):
-        g.node[node]["avg_year_out"] = 0
-    else:
-        g.node[node]["avg_year_out"] = weighted_sum / sum
-
-
-
-    weighted_sum = 0.0
-    sum = 0.0
-    for i in g.predecessors(node):
-        year1=int(i[-4:])
-        sum+=g[i][node]["weight"]
-        weighted_sum+=g[i][node]["weight"]*(year1-year)
-    g.node[node]["in"]=sum
-    if equal_zero(sum):
-        g.node[node]["avg_year_in"] = 0
-    else:
-        g.node[node]["avg_year_in"] = weighted_sum / sum
-
-#add a new attribute to sub_net_year
-def gen_avg_cited_net():
-    g=nx.read_gexf("data\\timeline_new_g.gexf")
-    for node in g:
-        gen_avg_cited_year(g,node)
-
-    nx.write_gexf(g,"data\\timeline_new_g.gexf")
-
-def gen_group_net(g,new_g,start_g):
-    for node in g.nodes(data=True):
-        if new_g.has_node(node[1]["group"]):
-            new_g.node[node[1]["group"]]["in"]+=node[1]["in"]
-            new_g.node[node[1]["group"]]["out"] += node[1]["out"]
-            new_g.node[node[1]["group"]]["num"] += node[1]["num"]
-            new_g.node[node[1]["group"]]["sum_author"] += node[1]["sum_author"]
-
-        else:
-            new_g.add_node(node[1]["group"], in_=node[1]["in"],out=node[1]["out"],num=node[1]["num"],
-                           weight=node[1]["weight"],label=node[1]["group"])
-    for edge in start_g.edges(data=True):
-       # print(g.node[start_g.node[edge[1]]["label"]])
-        node1=g.node[start_g.node[edge[0]]["label"]]["group"]
-        node2=g.node[start_g.node[edge[1]]["label"]]["group"]
-        if new_g.has_edge(node1,node2):
-            new_g[node1][node2]["weight"]+=edge[2]["weight"]
-        else:
-            new_g.add_edge(node1, node2, weight=edge[2]["weight"])
-
-def gen_new_net():
-    merge_multiName_net()
-    gen_avg_cited_net()
 
 class cal_imapct_factor:
     def __init__(self):
@@ -257,8 +363,9 @@ class cal_imapct_factor:
     def save(self):
         nx.write_gexf(self.g,"data\\timeline_new_g_l.gexf")
 
-a=cal_imapct_factor()
-a.cal_impact_factor()
+a=gen_net()
+a.gen_net_without_year()
+
 
 
 
